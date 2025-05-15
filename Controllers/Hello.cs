e67fbe522d61a69bc1fd5af647353609477dda7b
hanszxje <manh.luyen2512@gmail.com>
2025-05-15 16:20:55 +0700

    Add project files.

---
+++ Controllers/Hello.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace buoi3.Controllers
+{
+    public class Hello : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult hello()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace buoi3.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ShowKQ(string MSSV, string HoTen, float DiemTB, string ChuyenNganh)
+        {
+            danhSachSV.Add(new Student { MSSV = MSSV, HoTen = HoTen, DiemTB = DiemTB, ChuyenNganh = ChuyenNganh });
+            int soLuong = danhSachSV.Count(s => s.ChuyenNganh == ChuyenNganh);
+            ViewBag.MSSV = MSSV;
+            ViewBag.HoTen = HoTen;
+            ViewBag.DiemTB = DiemTB;
+            ViewBag.ChuyenNganh = ChuyenNganh;
+            ViewBag.SoLuong = soLuong;
+
+            return View();
+        }
+        private static List<Student> danhSachSV = new List<Student>();
+
+        public class Student
+        {
+            public string MSSV { get; set; }
+            public string HoTen { get; set; }
+            public float DiemTB { get; set; }
+            public string ChuyenNganh { get; set; }
+        }
+    }
+}
